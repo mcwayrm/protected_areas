cb050e31f4c93f1ff8d1fc128a98989f962fa142
Ryan McWay <ryan.mcway@gmail.com>
2023-02-14 14:03:17 -0600

    Example commit for Matt

--- 01_clean/01_test_clean_wdpa_df.R
+++ 01_clean/01_test_clean_wdpa_df.R
@@ -19,9 +19,9 @@ if (user == "ryanm") {
     # Ryan 
     setwd("C:/Users/ryanm/Dropbox (Personal)/Protected_Areas")
 }
-if (user == "") {
+if (user == "mbraaksma") {
     # Matt
-    setwd("C:/Users/..../Dropbox (Personal)/Protected_Areas")
+    setwd("/Users/mbraaksma/Dropbox/Protected_Areas")
 }
 
 #######################################################
@@ -32,6 +32,7 @@ if (user == "") {
 library(data.table)
 # Spatial Data 
 library(sf)
+library(tmap)
 
 
 #######################################################
@@ -39,6 +40,25 @@ library(sf)
 #######################################################
 
 # Bring in raw WDPA data
+gdb <- "./data/WDPA/raw/WDPA_Jan2023_Public.gdb"
+    # Check Layers within
+    st_layers(dsn = gdb) # Select the Polygons for the parks. 
+        # TODO: Will need to check out the point layer to see how this is different sometime
+wdpa_raw <- st_read(dsn = gdb, 
+                    # Only select Costa Rica parks
+                    query = "SELECT * FROM \"WDPA_poly_Jan2023\" WHERE ISO3 = 'CRI'")
+
+# Subset for only Central American countries 
+# iso3_ca <- c("BLZ","GTM","CRI", "PAN","HND","SLV","NIC")
+# wdpa <- wdpa_raw[wdpa_raw$ISO3 %in% iso3_ca,]
+# wdpa <- wdpa_raw[wdpa_raw$ISO3 == "CRI",]
+
+# drop invalid polygon: 555583004, General Use Zone, Turneffe Atoll Marine Reserve
+# wdpa[st_is_valid(wdpa) == FALSE,]
+wdpa <- wdpa[st_is_valid(wdpa) == TRUE,]
+
+tm_shape(wdpa) + tm_borders() + tmap_options(check.and.fix = TRUE)
+
 
 #######################################################
 # 02: Clean Data
@@ -56,5 +76,9 @@ library(sf)
 # 03: Save Cleaned Data
 #######################################################
 
-# Save 
+# Save Tabular data 
+write.csv()
+
+# Save Geometries 
+st_write()
 
