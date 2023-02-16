5c45e58a27506335ef0ea01e4436cd71eb08458f
Matt Braaksma <braaksma@protonmail.com>
2023-02-15 20:57:18 -0600

    TEST

--- 01_clean/01_test_clean_wdpa_df.R
+++ 01_clean/01_test_clean_wdpa_df.R
@@ -21,7 +21,7 @@ if (user == "ryanm") {
 }
 if (user == "mbraaksma") {
     # Matt
-    setwd("/Users/mbraaksma/Dropbox/Protected_Areas")
+    setwd("/Volumes/GoogleDrive-105356043703869020275/My Drive/Research/protected_areas")
 }
 
 #######################################################
@@ -41,21 +41,19 @@ library(tmap)
 
 # Bring in raw WDPA data
 gdb <- "./data/WDPA/raw/WDPA_Jan2023_Public.gdb"
+wdpa_raw <- st_read(dsn = gdb)
+                    
     # Check Layers within
-    st_layers(dsn = gdb) # Select the Polygons for the parks. 
-        # TODO: Will need to check out the point layer to see how this is different sometime
-wdpa_raw <- st_read(dsn = gdb, 
-                    # Only select Costa Rica parks
-                    query = "SELECT * FROM \"WDPA_poly_Jan2023\" WHERE ISO3 = 'CRI'")
+#     st_layers(dsn = gdb) # Select the Polygons for the parks. 
+#         # TODO: Will need to check out the point layer to see how this is different sometime
+# wdpa_raw <- st_read(dsn = gdb, 
+#                     # Only select Costa Rica parks
+#                     query = "SELECT * FROM \"WDPA_poly_Jan2023\" WHERE ISO3 = 'CRI'")
 
-# Subset for only Central American countries 
-# iso3_ca <- c("BLZ","GTM","CRI", "PAN","HND","SLV","NIC")
-# wdpa <- wdpa_raw[wdpa_raw$ISO3 %in% iso3_ca,]
-# wdpa <- wdpa_raw[wdpa_raw$ISO3 == "CRI",]
+# Subset to Costa Rica 
+wdpa <- wdpa_raw[wdpa_raw$ISO3 == "CRI",]
 
-# drop invalid polygon: 555583004, General Use Zone, Turneffe Atoll Marine Reserve
-# wdpa[st_is_valid(wdpa) == FALSE,]
-wdpa <- wdpa[st_is_valid(wdpa) == TRUE,]
+
 
 tm_shape(wdpa) + tm_borders() + tmap_options(check.and.fix = TRUE)
 
@@ -66,7 +64,11 @@ tm_shape(wdpa) + tm_borders() + tmap_options(check.and.fix = TRUE)
 
 # Only keep needed variables 
 
+
 # Remove problematic observations
+# check for invalid geometries
+sum(wdpa[st_is_valid(wdpa) == FALSE,])
+wdpa <- wdpa[st_is_valid(wdpa) == TRUE,]
 
 # Create generally useful information (dummies)
 
