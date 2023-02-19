3213f4ec4b6a2f74f1d6958839adbe108bfe0daf
Ryan McWay <ryan.mcway@gmail.com>
2023-02-19 14:29:41 -0600

    Minor edits to the WDPA cleaning

--- 01_clean/01_test_clean_wdpa_df.R
+++ 01_clean/01_test_clean_wdpa_df.R
@@ -21,7 +21,7 @@ if (user == "ryanm") {
 }
 if (user == "mbraaksma") {
     # Matt
-    setwd("/Users/mbraaksma/Dropbox/Protected_Areas")
+    setwd("/Volumes/GoogleDrive-105356043703869020275/My Drive/Research/protected_areas")
 }
 
 #######################################################
@@ -40,36 +40,43 @@ library(tmap)
 #######################################################
 
 # Bring in raw WDPA data
-gdb <- "./data/WDPA/raw/WDPA_Jan2023_Public.gdb"
+gdb <- "../data/WDPA/raw/WDPA_Jan2023_Public.gdb"
+
+wdpa_raw <- st_read(dsn = gdb)
+           
+# Query not working         
     # Check Layers within
-    st_layers(dsn = gdb) # Select the Polygons for the parks. 
-        # TODO: Will need to check out the point layer to see how this is different sometime
-wdpa_raw <- st_read(dsn = gdb, 
-                    # Only select Costa Rica parks
-                    query = "SELECT * FROM \"WDPA_poly_Jan2023\" WHERE ISO3 = 'CRI'")
+#     st_layers(dsn = gdb) # Select the Polygons for the parks. 
+#         # TODO: Will need to check out the point layer to see how this is different sometime
+# wdpa_raw <- st_read(dsn = gdb, 
+#                     # Only select Costa Rica parks
+#                     query = "SELECT * FROM \"WDPA_poly_Jan2023\" WHERE ISO3 = 'CRI'")
 
-# Subset for only Central American countries 
-# iso3_ca <- c("BLZ","GTM","CRI", "PAN","HND","SLV","NIC")
-# wdpa <- wdpa_raw[wdpa_raw$ISO3 %in% iso3_ca,]
-# wdpa <- wdpa_raw[wdpa_raw$ISO3 == "CRI",]
+# Subset to Costa Rica 
+wdpa <- wdpa_raw[wdpa_raw$ISO3 == "CRI",]
 
-# drop invalid polygon: 555583004, General Use Zone, Turneffe Atoll Marine Reserve
-# wdpa[st_is_valid(wdpa) == FALSE,]
-wdpa <- wdpa[st_is_valid(wdpa) == TRUE,]
-
-tm_shape(wdpa) + tm_borders() + tmap_options(check.and.fix = TRUE)
+# Plot
+tm_shape(wdpa) + tm_borders()
+tm_basemap(leaflet::providers$OpenStreetMap) + 
+    tm_shape(wdpa) + 
+    tm_borders(col = "red")
 
 
 #######################################################
 # 02: Clean Data
 #######################################################
 
-# Only keep needed variables 
-
 # Remove problematic observations
+# check for invalid geometries
+sum(wdpa[st_is_valid(wdpa) == FALSE,])
+wdpa <- wdpa[st_is_valid(wdpa) == TRUE,]
 
 # Create generally useful information (dummies)
+wdpa$ANY_MARINE <- ifelse(wdpa$MARINE > 0, 1, 0)
 
+# Only keep needed variables 
+wdpa_tab <- wdpa[, c("WDPAID", "STATUS_YR", "ANY_MARINE")]
+wdpa_geom <- wdpa[, c("WDPAID", "SHAPE")]
 
 
 #######################################################
@@ -77,8 +84,9 @@ tm_shape(wdpa) + tm_borders() + tmap_options(check.and.fix = TRUE)
 #######################################################
 
 # Save Tabular data 
-write.csv()
+st_write(wdpa_tab, "../data/WDPA/clean/wdpa_tab.csv")
 
 # Save Geometries 
-st_write()
+st_write(wdpa_geom, "../data/WDPA/clean/wdpa_geom.shp")
+
 
